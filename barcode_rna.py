2be98b5bfca093021737bc38cbe286fc41e9b061
nishuai <nishuai@yahoo.com>
2015-02-25 20:29:17 +0200

    Update barcode_rna.py

--- barcode_rna.py
+++ barcode_rna.py
@@ -9,14 +9,10 @@ def initiate_list():
 	for lines in table[1]:
 		globals()[lines]=[]
 	globals()['obsolete']=[]
-	print 'lists are initiated'
 ################# Define functions 
 def check_creat_path(path):
 	if not os.path.exists(os.path.dirname(path)):
-		print 'not exist'
 		os.makedirs(os.path.dirname(path))
-	else:
-		print '%s alrady exist' %path
 def write_list(listname,path):	
 		with open(path+'/'+listname+'.fastq', 'a+') as file:
 			for sublist in globals()[listname]:
@@ -32,7 +28,6 @@ for fastq in os.listdir(file_loci):
 			while True:
 				code = list(itertools.islice(input,4))
 				if code == []:
-					print 'file is over'
 					break
 				found = 0
 				for lines in table[1]:
@@ -47,7 +42,6 @@ for fastq in os.listdir(file_loci):
 					obsolete.append(code)
 ##### Obsolete contails the original barcode in order to ckeck later
 
-			print 'file %s is successfully processed, now start to write the files' % fastq
 			check_creat_path(file_loci+'classified/')
 			new_table=table[1][:]
 			new_table.append('obsolete')
@@ -57,7 +51,6 @@ for fastq in os.listdir(file_loci):
 			input.close()
 ####################### Rename file according to treatment
 for files in os.listdir(file_loci+'classified/'):
-	print files; print table[1];
 	if str(files)[:-6] in table[1]:
 		os.rename(file_loci+'classified/'+files,file_loci+'classified/'+table[0][table[1].index(str(files)[:-6])]+'.fastq')
 #
