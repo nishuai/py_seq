33143c8815c50a717665b6a25687a7f85a727087
Shuai Ni <shni@taito-login3.csc.fi>
2015-06-23 15:21:07 +0300

    similarity matrix for a give set of fasta sequences with biopython

--- pairwise_similarity_from_fasta.py
+++ pairwise_similarity_from_fasta.py
@@ -26,7 +26,11 @@ def seq_handshake(seqio_list):
 		for k in range(i+1, len(seqio_list)):
 			effective_length=min(len(seqio_list[i]),len(seqio_list[k]))
 			print sml_matrix
-			sml_matrix[i,k]=pairwise2.align.globalms(seqio_list[i].seq,seqio_list[k].seq,2,-1,-8,0,one_alignment_only=1,score_only=1)/(float(effective_length))
+			#### 2 points for perfect match, -1 for mismatch, -3 for gap opening, -1 for gap extension.
+			#### only_one_alignment and score_only spped up the calculation dramatically
+			#### gaps at the end of both sequences are not penalized to cope with large spam of length
+			sml_matrix[i,k]=pairwise2.align.globalms(seqio_list[i].seq,seqio_list[k].seq,2,-1,-3,-1,one_alignment_only=1,score_only=1,
+			penalize_end_gaps=0)/float(effective_length)
 			print i,k
 			print sml_matrix[i,k]
 	return sml_matrix, seq_names
@@ -34,6 +38,6 @@ def seq_handshake(seqio_list):
 ####
 similarity_matrix,seqname_list=seq_handshake(fastq_to_list(args.input_fastq))
 f=open('matrix.csv','w')
-f.write(','.join(seqname_list))
+f.write(','.join(seqname_list)+'\n')
 np.savetxt(f,similarity_matrix,delimiter=',')
 f.close()
