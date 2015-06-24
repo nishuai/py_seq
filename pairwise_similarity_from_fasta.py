3fbc0f7bb865e88a806989735c0e53833db9af0b
nishuai <nishuai@yahoo.com>
2015-06-24 14:34:38 +0300

    Update pairwise_similarity_from_fasta.py

--- pairwise_similarity_from_fasta.py
+++ pairwise_similarity_from_fasta.py
@@ -22,17 +22,13 @@ def seq_handshake(seqio_list):
 	sml_matrix=np.zeros(shape=(len(seqio_list), len(seqio_list)))
 	for i in range(len(seqio_list)):
 		seq_names.append(seqio_list[i].id)
-		print seq_names
 		for k in range(i+1, len(seqio_list)):
 			effective_length=min(len(seqio_list[i]),len(seqio_list[k]))
-			print sml_matrix
 			#### 2 points for perfect match, -1 for mismatch, -3 for gap opening, -1 for gap extension.
 			#### only_one_alignment and score_only spped up the calculation dramatically
 			#### gaps at the end of both sequences are not penalized to cope with large spam of length
 			sml_matrix[i,k]=pairwise2.align.globalms(seqio_list[i].seq,seqio_list[k].seq,2,-1,-3,-1,one_alignment_only=1,score_only=1,
 			penalize_end_gaps=0)/float(effective_length)
-			print i,k
-			print sml_matrix[i,k]
 	return sml_matrix, seq_names
 
 ####
